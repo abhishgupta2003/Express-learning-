42a125e28828f5dd54c824f31a490b730cf5ed37
Abhish <code.abhish@gmail.com>
2026-02-12 15:41:10 +0530

    profile added

--- first.js
+++ first.js
@@ -2,7 +2,10 @@ const express=require('express');
 const app=express();    
 const port=3000;
 app.get('/',(req,res)=>{
-    res.send('Hello World!  how are you ');
+    res.send('Hello World!  how are you  ');
+});
+app.get('/profile',(req,res)=>{
+    res.send('Hello World!  profile ');
 });
 app.listen(port, ()=>{
     console.log(`Server is running at http://localhost:${port}`);
