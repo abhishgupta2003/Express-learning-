49289ff3d17a76186c37608804ac22ac4869491c
Abhish <code.abhish@gmail.com>
2026-02-12 15:44:49 +0530

    updated profile

--- first.js
+++ first.js
@@ -5,7 +5,7 @@ app.get('/',(req,res)=>{
     res.send('Hello World!  how are you  ');
 });
 app.get('/profile',(req,res)=>{
-    res.send('Hello World!  profile ');
+    res.send('Hello World!  profile created successfully ');
 });
 app.listen(port, ()=>{
     console.log(`Server is running at http://localhost:${port}`);
